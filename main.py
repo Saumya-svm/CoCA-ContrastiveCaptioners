e0508dde1e5d4943ea8afa48e89609b918040fc8
Saumya Mundra <saumyamundra@gmail.com>
2024-07-02 12:35:25 +0530

    tested image encoder

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from coca import CoCa
-from transformers import ViTForImageClassification
+from transformers import ViTModel
+import torch
+from argparse import ArgumentParser
 
-image_enc = ViTForImageClassification.from_pretrained("google/vit-base-patch16-224")
-model = CoCa(image_enc=image_enc, vocab=['a', 'the', 'an'])
+def main(args):
+	image_enc = ViTModel.from_pretrained("google/vit-base-patch16-224")
+	model = CoCa(image_enc=image_enc, vocab=['a', 'the', 'an'])
+	
+	# print(model)
+	text = torch.randint(0, args.vocab_size, (args.batch_size, args.emb_dim))
+	images = torch.randn(args.batch_size, args.img_channels, args.img_size, args.img_size)
+	# print(images.shape)
+	out = model(text=text, images=images)
+	print(out)
 
-print(model)
+
+if __name__ == "__main__":
+	parser = ArgumentParser()
+	
+	parser.add_argument("--vocab_size", type=int, default=10000)
+	parser.add_argument("--batch_size", type=int, default=256)
+	parser.add_argument("--emb_dim", type=int, default=512)
+	parser.add_argument("--img_size", type=int, default=224)
+	parser.add_argument("--img_channels", type=int, default=3)
+
+	args = parser.parse_args()
+	main(args)
